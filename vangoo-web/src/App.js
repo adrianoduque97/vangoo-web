066b69244b1be858cdab5c57b979ed358c945b9a
Adrian Duque <adrianoduque97@gmail.com>
2020-11-16 17:17:23 -0500

    ADA -> Added first structure of the project

---
+++ vangoo-web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='./vangoo1.jpg' className="App-logo" alt="logo" />
+        <p>
+          VANGOO FIRST WEB
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
